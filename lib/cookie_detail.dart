81b6c6935200650388df49248e1f7bf1ad15bd72
John David Gonzalez <jhndagon@gmail.com>
2020-02-29 11:52:44 -0500

    flutter dojo

---
+++ lib/cookie_detail.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class CookieDetail extends StatelessWidget {
+  final assetPath, cookieprice, cookiename;
+
+  CookieDetail({this.assetPath, this.cookieprice, this.cookiename});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Color(0xFF545D68)),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: Text('Pickup',
+            style: TextStyle(
+                fontFamily: 'Varela',
+                fontSize: 20.0,
+                color: Color(0xFF545D68))),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.notifications_none, color: Color(0xFF545D68)),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: ListView(children: [
+        SizedBox(height: 15.0),
+        Padding(
+          padding: EdgeInsets.only(left: 20.0),
+          child: Text('Cookie',
+              style: TextStyle(
+                  fontFamily: 'Varela',
+                  fontSize: 42.0,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFFF17532))),
+        ),
+        SizedBox(height: 15.0),
+        Hero(
+            tag: assetPath,
+            child: Image.asset(assetPath,
+                height: 150.0, width: 100.0, fit: BoxFit.contain)),
+        Center(
+          child: Text(cookieprice,
+              style: TextStyle(
+                  fontFamily: 'Varela',
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFFF17532))),
+        ),
+        Center(
+          child: Text(cookiename,
+              style: TextStyle(
+                  color: Color(0xFF575E67),
+                  fontFamily: 'Varela',
+                  fontSize: 24.0)),
+        ),
+        
+      ]),
+    );
+  }
+}
